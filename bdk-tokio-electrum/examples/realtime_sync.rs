425e8c45cb0580232374bc67eb6bc267be01938b
Yuki Kishimoto <yukikishimoto@protonmail.com>
2026-03-25 17:23:20 +0100

    bdk: improve real-time sync example
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- bdk-tokio-electrum/examples/realtime_sync.rs
+++ bdk-tokio-electrum/examples/realtime_sync.rs
@@ -10,7 +10,7 @@ use tokio_electrum::prelude::*;
 async fn main() {
     // Initialize tracing for better diagnostics
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
+        .with_max_level(tracing::Level::DEBUG)
         .init();
 
     // Connect to Electrum server
@@ -27,33 +27,41 @@ async fn main() {
 
     let mut wallet = Wallet::create_from_two_path_descriptor("wpkh(tpubDDks68wKK1xKaVVVbNmXUAx68K1K817M6KwjvjEyCrjdU7xMvjKnfYAtZjfZcrfPfGFzqmibuVqMzKJGbBnK7mo7WSJri8Y9QgM7aNQ3fCp/<0;1>/*)").network(Network::Testnet4).create_wallet_no_persist().unwrap();
 
-    let request = wallet.start_full_scan().build();
-
-    println!("Starting full scan with subscriptions");
-
-    // Initial scan with subscriptions
-    let (initial_response, mut update_stream) = client
-        .full_scan_and_subscribe(request, 50, 5, false)
-        .await
-        .unwrap();
-
-    println!("Full scan completed");
-
-    // Apply initial scan to your wallet
-    wallet.apply_update(initial_response).unwrap();
-
-    println!("Listening for updates");
-
-    // Listen for real-time updates
-    while let Some(update_result) = update_stream.next().await {
-        match update_result {
-            Ok(update) => {
-                println!("Received tx update: {:?}", update);
-
-                // Apply update to wallet
-                wallet.apply_update(update).unwrap();
-            }
-            Err(e) => eprintln!("Update error: {}", e),
+    loop {
+        if !client.status().is_connected() {
+            tokio::time::sleep(Duration::from_secs(3)).await;
+            continue;
         }
+
+        println!("Starting full scan with subscriptions");
+
+        // Initial scan with subscriptions
+        let request = wallet.start_full_scan().build();
+        let (initial_response, mut update_stream) = client
+            .full_scan_and_subscribe(request, 50, 5, false)
+            .await
+            .unwrap();
+
+        println!("Full scan completed");
+
+        // Apply initial scan to your wallet
+        wallet.apply_update(initial_response).unwrap();
+
+        println!("Listening for updates");
+
+        // Listen for real-time updates
+        while let Some(update_result) = update_stream.next().await {
+            match update_result {
+                Ok(update) => {
+                    println!("Received tx update: {:?}", update);
+
+                    // Apply update to wallet
+                    wallet.apply_update(update).unwrap();
+                }
+                Err(e) => eprintln!("Update error: {}", e),
+            }
+        }
+
+        println!("Streaming terminated. Waiting for connection...");
     }
 }
